9aa44f27d6aa73261efd345b5c91b9fabe66a9ab
霜羽 HoarfrostFeather <penguin.zhang@qq.com>
2021-02-16 16:05:49 +0800

    Update Workflow

--- .github/sh/page-generator.sh
+++ .github/sh/page-generator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ../../
 for f in documents/*.md
-  do if [ ! -f "./archive/${${f//md/html}//documents\//}" ]
+  do if [ ! -f "./archive/$("$(f//md/html)"//documents//)" ]
   then
-  echo "Generate ${f//md/html}"
-  md2html $f -o ./archive; fi
-  ls-minifier $f --silent --override --html-compressor=html-minifier
+  echo "Generate $(f//md/html)"
+  md2html "$f" -o ./archive; fi
+  ls-minifier "$f" --silent --override --html-compressor=html-minifier
   done

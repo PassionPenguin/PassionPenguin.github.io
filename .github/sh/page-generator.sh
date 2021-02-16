ef558aa7787b21ec53ab1564956fac27f47f480d
霜羽 HoarfrostFeather <penguin.zhang@qq.com>
2021-02-16 22:38:12 +0800

    Update Workflow

--- .github/sh/page-generator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ../../
-for f in documents/*.md
-  do if [ ! -f "./archive/$("$(f//md/html)"//documents//)" ]
-  then
-  echo "Generate $(f//md/html)"
-  md2html "$f" -o ./archive; fi
-  ls-minifier "$f" --silent --override --html-compressor=html-minifier
-  done

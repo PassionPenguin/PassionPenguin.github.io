aacc2332f5d6e7f48a71d87355e8e765a00f3cbf
霜羽 HoarfrostFeather <penguin.zhang@qq.com>
2021-02-16 16:01:26 +0800

    Update Workflow

---
+++ .github/sh/page-generator.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+cd ../../
+for f in documents/*.md
+  do if [ ! -f "./archive/${${f//md/html}//documents\//}" ]
+  then
+  echo "Generate ${f//md/html}"
+  md2html $f -o ./archive; fi
+  ls-minifier $f --silent --override --html-compressor=html-minifier
+  done
